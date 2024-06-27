e94ef9a4ea6f97a5e62f3a01b337c4a284ed9be0
Dowgojashan <134786599+Dowgojashan@users.noreply.github.com>
2024-06-28 01:31:48 +0800

    update

---
+++ app/src/main/java/com/example/life_assistant/data/Event.kt
@@ -0,0 +1,10 @@
+package com.example.life_assistant.data
+
+data class Event(
+    val name: String = "",
+    val description: String = "",
+    val date: String,
+//    val label: String = "",
+//    val remind_time: Long,
+//    val repeat: Int,
+)
