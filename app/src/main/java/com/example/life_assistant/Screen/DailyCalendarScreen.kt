e9fddb708452a9f2faa52964578335096727c1c2
alicia <90881844+alicia0725@users.noreply.github.com>
2024-07-29 09:46:14 +0800

    time

--- app/src/main/java/com/example/life_assistant/Screen/DailyCalendarScreen.kt
+++ app/src/main/java/com/example/life_assistant/Screen/DailyCalendarScreen.kt
@@ -675,7 +675,7 @@ fun UserInputDialog(
     var deadline by remember { mutableStateOf("") }
     var duration by remember { mutableStateOf("") }
     var isSplittable by remember { mutableStateOf(false) }
-
+    val needState = rememberTimePickerState(0, 0, true)
     Log.d("date","$selectedDay")
 
     // Function to format the date to a string
@@ -877,7 +877,7 @@ fun UserInputDialog(
                             Spacer(modifier = Modifier.height(8.dp))
 
                             Row(verticalAlignment = Alignment.CenterVertically) {
-                                Text("結束時間:", color = Color.Black)//這邊有一個小小的要抓，結束時間最早只能是當天
+                                Text("截止日期:", color = Color.Black)//這邊有一個小小的要抓，結束時間最早只能是當天
                                 Spacer(modifier = Modifier.width(8.dp))
                                 Button(
                                     onClick = { showDatePicker = true },
@@ -911,12 +911,21 @@ fun UserInputDialog(
                             Row(verticalAlignment = Alignment.CenterVertically) {
                                 Text("所需時間:", color = Color.Black)
                                 Spacer(modifier = Modifier.width(8.dp))
-                                TextField(
-                                    value = duration,
-                                    onValueChange = { duration = it },
-                                    modifier = Modifier.fillMaxWidth()
+                                TimeInput(
+                                    state = needState,
+                                    colors = TimePickerDefaults.colors(
+                                        timeSelectorSelectedContainerColor = Color(0xffb4cfe2),
+                                        timeSelectorSelectedContentColor = Color.Black,
+                                        timeSelectorUnselectedContainerColor = Color(0xffb4cfe2),
+                                        timeSelectorUnselectedContentColor = Color.Black
+                                    ),
+                                    modifier = Modifier
+                                        .width(200.dp)
+                                        .height(100.dp) // Adjust the width and height as needed
                                 )
                             }
+
+                        }
 
                             Spacer(modifier = Modifier.height(8.dp))
 
@@ -1067,7 +1076,6 @@ fun UserInputDialog(
             }
         }
     }
-}
 
 //抓錯誤視窗
 @Composable
